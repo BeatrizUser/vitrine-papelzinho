1350792c54fb6baf112d969c961278c99d69d19c
BeatrizUser <beatriz.machado@orla.tech>
2024-07-19 14:54:13 -0300

    vitrine papelzinho

---
+++ src/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const data = "../data/produtos.json";
+    fetch(data)
+        .then(response => response.json())
+        .then(data => {
+            const productList = document.getElementById('product-list');
+            data.forEach(product => {
+                const productCard = `
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <img src="${product.image}" class="card-img-top" alt="${product.name}">
+                            <div class="card-body">
+                                <h5 class="card-title">${product.name}</h5>
+                                <p class="card-text">${product.description}</p>
+                                <a href="${product.detailsPage}" class="btn btn-papelzinho">Ver Detalhes</a>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                productList.innerHTML += productCard;
+            });
+        })
+        .catch(error => console.error('Erro ao carregar produtos:', error));
+});
